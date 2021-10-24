d98ae5bae612775425462329b9dfcdf5167ea0ac
insn0w <92552519+insn0w@users.noreply.github.com>
2021-10-24 18:28:10 +0300

    Update deploy.py

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -2,34 +2,35 @@ name: Deployment-app
 on: 
   pull-request:
 jobs: 
-  build-and-deploy:
-    matrix:
+  name: build-and-deploy:
+    strategy:
+      matrix:
         os: [ubuntu-latest, macos-latest]
     runs-on: ${{ matrix.os }}
     steps:
-    - run: echo "This has been triggered $ {{ githusb.event_name }}
-    - run: echo "This job is runining on $ {{ runer.os }} server hosted on github!"
-    - name: Check out repository code
-      uses: actions/checkout@v2
-    - name: Set up Python 3.9
-      uses: actions/setup-python@v2
-      with:
-        python-version: 3.9
-    - name: Install dependencies
-      run: |
-        python -m pip install --upgrade pip
-        pip install pylint
-    - name: Lint with pylint
-      run: |
-        python lint_test.py
-    - name: Build container
-      run: |
-        echo "Building a container image..."
-        sleep 30
-   - name: Deploy to cluster
+      - run: echo "This has been triggered $ {{ githusb.event_name }}
+      - run: echo "This job is runining on $ {{ runer.os }} server hosted on github!"
+      - name: Check out repository code
+        uses: actions/checkout@v2
+      - name: Set up Python 3.9
+        uses: actions/setup-python@v2
+        with:
+          python-version: 3.9
+      - name: Install dependencies
+        run: |
+          python -m pip install --upgrade pip
+          pip install pylint
+      - name: Lint with pylint
+        run: |
+          python lint_test.py
+      - name: Build container
+        run: |
+          echo "Building a container image..."
+          sleep 30
+     - name: Deploy to cluster
        run: |
          echo "Deployning..."
          sleep 30
          echo "Conteiner has been deployd!"
-        echo This job's status is ${{ jobs.status }}." 
+     - run: echo This job's status is ${{ jobs.status }}." 
      
